dc3b26c89d4e7fa3cb32ac3a4c1385873079529e
José Rodrigo Abril Lara <rabril@worth.systems>
2026-03-04 10:52:02 +0000

    style: Remove how-to-pay page
    closes #115

--- app/main/views_nl/sub_navigation_dictionaries.py
+++ app/main/views_nl/sub_navigation_dictionaries.py
@@ -51,10 +51,10 @@ def pricing_nav():
                     "name": "Brieven",
                     "link": "main.guidance_pricing_letters",
                 },
-                {
-                    "name": "Hoe u kunt betalen",
-                    "link": "main.guidance_how_to_pay",
-                },
+                # {
+                #     "name": "Hoe u kunt betalen",
+                #     "link": "main.guidance_how_to_pay",
+                # },
                 {
                     "name": "Factureringsgegevens",
                     "link": "main.guidance_billing_details",
